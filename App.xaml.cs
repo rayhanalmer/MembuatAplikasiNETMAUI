147c6010321a54da50adc19a0c650db9049fe58f
rayhanalmer <103409628+rayhanalmer@users.noreply.github.com>
2023-10-15 15:06:04 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MembuatAplikasiNETMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MembuatAplikasiNETMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
